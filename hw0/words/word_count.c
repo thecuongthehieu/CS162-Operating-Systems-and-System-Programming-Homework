98657b8393a62c39eea18d408adf40e26aa61160
Alan Ton <alanton@berkeley.edu>
2020-01-22 01:17:41 +0000

    [HW0] Initial skeleton code

--- hw0/words/word_count.c
+++ hw0/words/word_count.c
@@ -1,4 +1,32 @@
+/*
+
+Copyright © 2019 University of California, Berkeley
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+word_count provides lists of words and associated count
+
+Functional methods take the head of a list as first arg.
+Mutators take a reference to a list as first arg.
+*/
+
 #include "word_count.h"
+
+/* Basic utililties */
+
+char *new_string(char *str) {
+  return strcpy((char *)malloc(strlen(str)+1), str);
+}
 
 void init_words(WordCount **wclist) {
   /* Initialize word count.  */
@@ -6,14 +34,18 @@ void init_words(WordCount **wclist) {
 }
 
 size_t len_words(WordCount *wchead) {
-  return 0;
+    size_t len = 0;
+    return len;
 }
 
 WordCount *find_word(WordCount *wchead, char *word) {
-  return NULL;
+  /* Return count for word, if it exists */
+  WordCount *wc = NULL;
+  return wc;
 }
 
 void add_word(WordCount **wclist, char *word) {
+  /* If word is present in word_counts list, increment the count, otw insert with count 1. */
 }
 
 void fprint_words(WordCount *wchead, FILE *ofile) {
@@ -28,8 +60,6 @@ void fprint_words(WordCount *wchead, FILE *ofile) {
 sort_words is provided for you in the object file wc_sort.o
 If you want to do the *stretch* implement it here and comment
 out the wc_sort library in the Makefile
-
 void sort_words(word_count **words, int (*comparator)(word_count *, word_count *)) {
-
 }
 */
