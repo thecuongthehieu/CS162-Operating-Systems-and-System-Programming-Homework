071bc7a0f7d7770863e0c23d586795b045a670ae
Alex <alexthomas@berkeley.edu>
2020-01-23 21:36:15 +0000

    Updated to correct starter files

--- hw0/recurse.c
+++ hw0/recurse.c
@@ -1,6 +1,9 @@
+#include <stdio.h>
+
 int recur(int i) {
     /* A stack allocated variable within a recursive function */
     int j = i;
+    printf("%i\n", i);
 
     if (i > 0) {
         return recur(i - 1);
