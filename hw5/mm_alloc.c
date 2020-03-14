01b9685a902d6f8f373e93c8cdcaa13b8b82ba2b
Sarah Sun <sarahsun@berkeley.edu>
2020-03-14 01:16:02 -0700

    Added hw5

---
+++ hw5/mm_alloc.c
@@ -0,0 +1,32 @@
+/*
+ * mm_alloc.c
+ */
+
+#include "mm_alloc.h"
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+
+void* mm_malloc(size_t size)
+{
+  //TODO: Implement malloc
+
+  return NULL;
+}
+
+void* mm_realloc(void* ptr, size_t size)
+{
+  //TODO: Implement realloc
+
+  return NULL;
+}
+
+void mm_free(void* ptr)
+{
+  //TODO: Implement free
+
+  return NULL;
+}
