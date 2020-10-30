0cddc2947c580365f530259ffa060fa8250079fe
thecuong <thecuongthehieu@gmail.com>
2020-10-30 16:43:12 +0700

    HW5: Malloc

--- hw5/mm_alloc.c
+++ hw5/mm_alloc.c
@@ -9,24 +9,179 @@
 #include <stdio.h>
 #include <string.h>
 
+s_block_ptr HeadPtr=NULL;
 
-void* mm_malloc(size_t size)
-{
-  //TODO: Implement malloc
-
-  return NULL;
+s_block_ptr extend_heap (s_block_ptr last , size_t s){
+	void* p=sbrk(s+sizeof(s_block));
+		
+	if(!(p==(void *)-1)){
+		
+		s_block_ptr newBlock = (s_block_ptr) p;
+		if(last){
+			last->next=newBlock;
+			
+		}else{
+			HeadPtr=newBlock;
+		}
+		newBlock->prev=last;
+		newBlock->next=NULL;
+		newBlock->size=s;
+		newBlock->block=p+sizeof(s_block);
+		//last->ptr=p;
+		newBlock->free=0;
+		
+		return newBlock;
+	}
+	//Return Null on error
+	return NULL;
 }
 
-void* mm_realloc(void* ptr, size_t size)
-{
-  //TODO: Implement realloc
-
-  return NULL;
+s_block_ptr get_block (void *p){
+	s_block_ptr head=HeadPtr;
+	
+	while(head){
+		if(head->block==p){
+			return head;
+		}
+		head=head->next;
+	}
+	//null if we cant find it.
+	return NULL;
+	
 }
 
-void mm_free(void* ptr)
-{
-  //TODO: Implement free
-
-  return NULL;
+s_block_ptr fusion(s_block_ptr b){
+	if( (b->next)->free ==1 ){
+		b->size=b->size+sizeof(s_block)+(b->next)->size;
+		b->next=(b->next)->next;
+		(b->next)->prev=b;
+	}
+	
+	if( (b->prev)->free ==1 ){
+		
+		(b->prev)->next=b->next;
+		(b->next)->prev=b->prev;
+		(b->prev)->free=b->free;
+		(b->prev)->size=(b->prev)->size + sizeof(s_block) + b->size;
+		
+		return b->prev;
+	}
+	
+	return b;
 }
+
+void split_block (s_block_ptr b, size_t s){
+	if(b && s >= sizeof(void *)){
+		if(  b->size - s  >= sizeof(s_block) + sizeof(void*) ){
+			s_block_ptr newP = (s_block_ptr) (b->block + s);
+			newP->next = b->next;
+			(newP->next)->prev=newP;
+			b->next=newP;
+			newP->prev=b;
+			newP->size=b->size - s - sizeof(s_block);
+			b->size = s;
+			newP->block= b->block + s + sizeof(s_block);
+			
+			mm_free(newP->block);
+			
+		}
+	}
+}
+
+void* mem_copy(s_block_ptr oldB, s_block_ptr newB){
+	if( oldB && newB){
+		char * oStart= (char *) oldB->block;
+		char * nStart=(char *) newB->block;
+		int i;
+		for(i=0;i<oldB->size;i++){
+			*(nStart + i)=*(oStart + i);
+		}
+		
+		return newB->block;
+		
+	}
+	
+	return NULL;
+}
+
+
+void* mm_malloc(size_t size){
+	
+	s_block_ptr head=HeadPtr;
+	s_block_ptr prev=NULL;
+	
+	while(head){
+		if(head->free==1 && head->size >=size){
+			mm_realloc(head->block,size);
+			head->free=0;
+			return head->block;
+			
+		}else{
+			prev=head;
+			head=head->next;
+		}
+	}
+	
+	//If we exited the loop, we know that we didnt find a suitable space, and so we need to move the break pt.
+	head=extend_heap(prev,size);
+	if(!head){
+		exit(EXIT_FAILURE);
+	}
+	return head->block;
+	
+}
+
+/*mm_realloc:
+ * If want bigger space then acts the same as malloc but copies over contents of old block to a new, more
+ * approproate block.
+ * If smaller block is needed but current block isnt big enough to split into two viable blocks, then nothing is
+ * done.
+ * If current clock is big enough then the block is split accordingly
+ */
+void* mm_realloc(void* ptr, size_t size){
+		s_block_ptr curr=get_block(ptr);
+		
+		if(curr){
+			if(size > curr->size){
+				void* p=mm_malloc(size);
+				s_block_ptr newP=get_block(p);
+				if(newP){
+					p=mem_copy(curr,newP);
+					mm_free(curr->block);
+					return p;
+				}
+				
+				
+					
+			}else if(size < curr->size){
+				split_block(curr,size);
+				return curr->block;
+			}else{
+				return curr->block;
+			}
+		}
+		return NULL;
+}
+
+void mm_free(void* ptr){
+	s_block_ptr curr=get_block(ptr);
+	
+	if(curr){
+		if(curr->next==NULL){
+			
+			if(curr->prev){
+				
+				(curr->prev)->next=NULL;
+			}else{
+				HeadPtr=NULL;
+			}
+			
+			sbrk(-(curr->size + sizeof(s_block)));
+		}else{
+			
+			curr->free=1;
+			fusion(curr);
+		}
+	}
+}
+
--- hw5/mm_alloc.h
+++ hw5/mm_alloc.h
@@ -9,6 +9,9 @@
 #ifndef _malloc_H_
 #define _malloc_H_
 
+/* Define the block size since the sizeof will be wrong */
+#define BLOCK_SIZE 40
+
 #include <stdlib.h>
 
 void* mm_malloc(size_t size);
@@ -16,5 +19,46 @@ void* mm_realloc(void* ptr, size_t size);
 void mm_free(void* ptr);
 
 //TODO: Add any implementation details you might need to this file
+
+typedef struct s_block *s_block_ptr;
+extern s_block_ptr HeadPtr;
+
+/* block struct */
+typedef struct s_block {
+    size_t size; //8 bytes
+    struct s_block *next; //8bytes
+    struct s_block *prev; //8bytes
+    int free; //4 bytes
+    void *block; //8bytes
+    
+    //There will padding here of 4 bytes (on my 64 bit machine), but I like this method even though it wastes 4
+    //bytes because it makes the 
+    //program more cross platform since sizeof can be used.
+    //void *ptr;
+    /* A pointer to the allocated block */
+    //char data[0];
+    
+ }s_block;
+
+
+/* Split block according to size, b must exist */
+void split_block (s_block_ptr b, size_t s);
+
+/* Try fusing block with neighbors */
+s_block_ptr fusion(s_block_ptr b);
+
+/* Get the block from addr */
+s_block_ptr get_block (void *p);
+
+/* Add a new block at the of heap,
+ * return NULL if things go wrong
+ */
+s_block_ptr extend_heap (s_block_ptr last , size_t s);
+
+/* Copy info and contents of block from one location to another.
+ * returns ptr to new block if successful or NULL otherwise
+ */
+void* mem_copy(s_block_ptr oldB, s_block_ptr newB);
+
 
 #endif
